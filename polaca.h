b2b8c2a73393fb5acbf0d56aa00282aa92ced9b9
santiago.segnini <santiago.segnini@gmail.com>
2013-11-02 20:27:06 +0000

    Incorporada polaca para IFs

--- polaca.h
+++ polaca.h
@@ -1,33 +1,59 @@
-#define TAM_POLACA 4000
+#define TAMANIO_COLA 10000
 
-#include "pila.h"
+struct elementoPolaca {
+	char elemento[40];
+	char tipo;
+};
 
+typedef struct colaPolaca {
+	struct elementoPolaca elementos[TAMANIO_COLA];
+	int cantidadElementosCola;
+} colaPolaca;
 
-	
-void polacaAgregar(struct pila * pol,char * elemento, char tipo) {
+void polacaAgregar(colaPolaca * pol,char * elemento, char tipo, int posicion) {
 	struct elementoPolaca nuevo;
 	strcpy(nuevo.elemento,elemento);
 	nuevo.tipo=tipo;
-	
-	pilaEmpujar(pol,nuevo);
+	colaEmpujar(pol,nuevo, posicion);
 }
 
+void colaEmpujar(colaPolaca * stack, struct elementoPolaca elemento, int posicion) {
 
-void imprimePolaca(struct pila * pol)
-{
-	imprimirPila(pol);
-
-	/*int i=0;
-	printf("Abriendo archivo de intermedia\n");
-	FILE *out = fopen("intermedia.txt","w");
-	printf("Abierto archivo de intermedia. Volcando %d elementos \n",pol->cantidadElementos);
-	
-	while(i<pol->cantidadElementos)
-	{
-		fprintf(out,"%s (%c) \n",pol->elementos[i].elemento,pol->elementos[i].tipo);
-		i++;
+	if(stack->cantidadElementosCola==TAMANIO_COLA) {
+		printf("Stack Overflow!");
+		exit(1);
+		}
+		
+	if(posicion==-1) {
+		stack->elementos[stack->cantidadElementosCola]=elemento;
+		stack->cantidadElementosCola++;
 	}
-	
-	fclose(out);*/
+	else {
+		printf("\nIntentando insertar en posicion %d\n",posicion);
+		stack->elementos[posicion]=elemento;
+	}
 }
 
+int colaVacia (colaPolaca * stack) {
+	if(stack->cantidadElementosCola==0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+void imprimirCola(colaPolaca * stack) {
+	printf("\n===VOLCADO DE POLACA=== \n");
+	int i=0;
+	while(i<stack->cantidadElementosCola) {
+		struct elementoPolaca elem = stack->elementos[i-1];
+		printf("POS: %d - %s (%c)\n",i,elem.elemento,elem.tipo);	
+		i++;
+	} 
+}
+
+void imprimePolaca(colaPolaca * pol)
+{
+	imprimirCola(pol);
+}
